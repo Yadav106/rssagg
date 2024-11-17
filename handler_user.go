5a37feb1e8fc3359bc44e7a1109d9106dcb9eda1
Yadav106 <sahil.jp.yadav@gmail.com>
2024-11-17 14:49:11 +0530

    added middleware for auth

--- handler_user.go
+++ handler_user.go
@@ -6,49 +6,41 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/Yadav106/rssagg/internal/auth"
 	"github.com/Yadav106/rssagg/internal/database"
 	"github.com/google/uuid"
 )
 
 func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
-  type parameters struct {
-    Name string `json:"name"`
-  }
+	type parameters struct {
+		Name string `json:"name"`
+	}
 
-  decoder := json.NewDecoder(r.Body)
+	decoder := json.NewDecoder(r.Body)
 
-  params := parameters{}
-  err := decoder.Decode(&params)
-  if err != nil {
-    respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
-    return
-  }
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+		return
+	}
 
-  user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
-    ID: uuid.New(),
-    CreatedAt: time.Now().UTC(),
-    UpdatedAt: time.Now().UTC(),
-    Name: params.Name,
-  })
-  if err != nil {
-    respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
-  }
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
+	}
 
-  respondWithJSON(w, 201, DatabaseUserToUser(user))
+	respondWithJSON(w, 201, DatabaseUserToUser(user))
 }
 
-func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
-  apiKey, err := auth.GetAPIKey(r.Header)
-  if err != nil {
-    respondWithError(w, 403, fmt.Sprintf("Auth Error: %v", err))
-  }
-
-  user, err := apiCfg.DB.GetUserByApiKey(r.Context(), apiKey)
-  if err != nil {
-    respondWithError(w, 403, fmt.Sprintf("Couldn't get user: %v", err))
-    return
-  }
-
-  respondWithJSON(w, 200, DatabaseUserToUser(user))
+func (apiCfg *apiConfig) handlerGetUser(
+  w http.ResponseWriter,
+	r *http.Request,
+	user database.User,
+) {
+	respondWithJSON(w, 200, DatabaseUserToUser(user))
 }
