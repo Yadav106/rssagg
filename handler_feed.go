453be79fdb996c412ac6dceb39d2545d421734af
Yadav106 <sahil.jp.yadav@gmail.com>
2024-11-17 16:23:32 +0530

    added api for get all feeds

--- handler_feed.go
+++ handler_feed.go
@@ -41,3 +41,15 @@ func (apiCfg *apiConfig) handlerCreateFeed(
 
 	respondWithJSON(w, 201, DatabaseFeedToFeed(feed))
 }
+
+func (apiCfg *apiConfig) handlerGetFeeds(
+	w http.ResponseWriter, r *http.Request,
+) {
+
+	feeds, err := apiCfg.DB.GetFeeds(r.Context())
+  if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get feeds: %v", err))
+	}
+
+	respondWithJSON(w, 201, DatabaseFeedsToFeeds(feeds))
+}
